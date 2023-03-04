fdf043a371a9bcb1f429657b2b5990f4565e7d2e
Gustavo Santos <95867198+Gustalex@users.noreply.github.com>
2023-03-04 10:24:20 -0300

    Add files via upload

---
+++ escapesequences.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+int main(){
+    //This is a comment                    /* This is a multiline comment*/  
+    
+    /* escape sequence = a character combination of a backslash \
+                            followed by a letter or a combination of digitis.
+                            They specify actions within a line or string of text.
+                            \n = new line 
+                            \t = tab
+                            \a	07	Alert (Beep, Bell) (added in C89)[1]
+                            \b	08	Backspace
+                            \e 	1B	Escape character
+                            \f	0C	Formfeed Page Break
+                            \n	0A	Newline (Line Feed); see notes below
+                            \r	0D	Carriage Return
+                            \t	09	Horizontal Tab
+                            \v	0B	Vertical Tab
+                            \\	5C	Backslash
+                            \'	27	Apostrophe or single quotation mark
+                            \"	22	Double quotation mark
+                            \?	3F	Question mark (used to avoid trigraphs) 
+    */
+    
+    printf("1\t2\t3");                                                                                 
+    return 0;
+   
+}
