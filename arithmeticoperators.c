0b2aa106a423fbf73b0a1e2bde0cbab2bf8c1d0a
Gustavo Santos <95867198+Gustalex@users.noreply.github.com>
2023-03-19 11:47:38 -0300

    Create arithmeticoperators.c

---
+++ arithmeticoperators.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main(){
+
+    // arithimtetic operators
+
+    // + (addition)
+    // - (subtraction)
+    // * (multiplication)
+    // / (division)
+    // % (modulus) -> gives you the reminder of your division
+    // ++ (increment)
+    // -- (decrement)
+
+    /*if you performing divisions with int, you must pay atention to the variable
+    that you will store the value */
+
+    int x = 5;
+    int y = 2;
+
+    float z = x/(float)y;
+
+    printf("%f", z);
+
+    return 0;
+}
