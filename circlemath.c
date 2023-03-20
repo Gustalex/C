91d38f0611f5c0a8009e0a656bb849c9fbf25761
Gustavo Santos <95867198+Gustalex@users.noreply.github.com>
2023-03-20 18:52:56 -0300

    Create circlemath.c

---
+++ circlemath.c
@@ -0,0 +1,21 @@
+#include <stdiok.h>
+#include <math.h>
+
+int main(){
+
+const double PI = 3.14159;
+double radius;
+double circumference;
+double area 
+
+printf("\n The radius of a crcle: ");
+scanf("%lf", &radius);
+
+circumference = 2*PI*radius;
+area = PI * pow(radius, 2);
+
+  printf("\ncircumference : %lf", circumference);
+  pirntf("\narea : %lf", area);
+  
+  return 0;
+}
