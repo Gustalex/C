3a1d1b78882001d7d3ee64fe717c2e9769564c0f
Gustavo Santos <95867198+Gustalex@users.noreply.github.com>
2023-03-29 10:11:55 -0300

    Create basicfunction.c

---
+++ basicfunction.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+void birthday()
+{
+    printf("\nHappy birthday to you");
+    printf("\nHappy birthday to you");
+    printf("\nHappy birthday dear ...");
+    printf("\nHappy birthday to you");
+}
+
+int main (){
+
+    // function is a small section of code that is executed whenever we call it 
+
+    birthday();
+    birthday();
+    birthday();
+
+    return 0;
+}
