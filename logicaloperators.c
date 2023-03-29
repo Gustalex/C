48cbbd09935d32173ab2e1c0d43eaef177b77948
Gustavo Santos <95867198+Gustalex@users.noreply.github.com>
2023-03-29 09:59:00 -0300

    Create logicaloperators.c

---
+++ logicaloperators.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+#include <stdbool.h>
+/*int main (){
+
+// logical operators == && (AND) check if two conditions are true
+        
+    float temp = 25;
+    bool sunny = true;
+
+    if (temp >=0 && temp <=30 && sunny){
+        printf("\nThe weather is good!");
+    }
+    else{
+        printf("\nThe weather is bad!");
+    }
+
+
+
+    return 0;
+}*/
+
+
+
+/*int main (){
+
+    // logical operators  == || (OR) checks if at least one condition is true 
+
+    float temp = 31;
+
+    if (temp<=0 || temp>=30)
+    {
+        printf("\nThe weather is bad!");
+    }
+    else{
+        printf("\nThe weather is good!");
+    }
+
+
+    return 0;
+}*/
+
+
+int main(){
+
+    // logical operators == ! (NOT) reverses the state of a condition
+
+    bool sunny = true;
+
+    if (!sunny == 1 )
+    {
+        printf("\n It's sunny outside!");
+    }
+    else{
+        printf("It's cloudy outside!");
+    }
+
+
+    return 0;
+}
