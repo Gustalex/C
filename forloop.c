75a5240399c3f2a892a30ed5d74eaf7da2b51a03
Gustavo Santos <95867198+Gustalex@users.noreply.github.com>
2023-04-04 20:44:28 -0300

    Create forloop.c

---
+++ forloop.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    // for loop = repeats a section of code a limited amount of times 
+
+    for (int i =1; i <=10; i++) //i+=2(incremet by 2) i+=3(incremet by 3)...
+    {
+       printf("\n%d", i);
+    }
+    
+
+    return 0;
+}
